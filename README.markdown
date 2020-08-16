75d682a9caac551e3c6ddf794a44c0b16c3b9d34
Luke Channings <luke@channings.me>
2020-08-16 13:44:21 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,38 @@
+# name-long-afix-component
+
+description-afix-component
+
+## Using it
+
+Try it out directly (requires a modern browser)
+
+```html
+<script src="https://cdn.skypack.dev/name-afix-component@latest" type="module"></script>
+```
+
+Or install with
+
+```bash
+npm install name-afix-component
+```
+
+And use like this
+
+```js
+import "name-afix-component"`
+```
+
+### Attributes
+
+|Name|Description|Default|
+|-|-|-|
+
+### Slots
+
+|Name|Description|Default|
+|-|-|-|
+
+### CSS Custom Properties
+
+|Name|Description|Default|
+|-|-|-|
---
+++ src/Component.js
@@ -0,0 +1,32 @@
+const template = html`
+  <h1>Hello world</h1>
+  <style>
+    h1 {
+      font-size: xx-large;
+      color: red;
+      font-family: "Comic Sans MS", cursive, sans-serif;
+    }
+  </style>
+`;
+
+export default class Component extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" }).appendChild(
+      template.content.cloneNode(true)
+    );
+  }
+}
+
+customElements.define("name-afix-component", Component);
+
+/**
+ * makeTemplate is a template tag used to construct a <template>.
+ * @param {TemplateStringsArray} htmlString
+ */
+function html(htmlString) {
+  const template = document.createElement("template");
+  template.innerHTML = String.raw(htmlString);
+  return template;
+}
